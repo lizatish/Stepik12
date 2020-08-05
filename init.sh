c4c76952f70dacd7d0de798a6a99af242ac7bc0b
Liza Tishkovets <tiiska2@gmail.com>
2020-08-05 16:49:02 +0300

    Init

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo -c /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello
 sudo /etc/init.d/nginx restart
 
