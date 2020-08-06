72c8f601e757bb2f281e8a7901ef188b8b33f65d
Liza Tishkovets <tiiska2@gmail.com>
2020-08-06 15:19:12 +0300

    Working second task

--- hello.py
+++ hello.py
@@ -2,7 +2,6 @@ def wsgi_appliction(environ, start_response):
     raw_uri = str(environ.get('RAW_URI'))
     raw_uri = raw_uri[2:]
     params = raw_uri.split('&')
-
     data = ''
     for param in params:
         data += param + '\r\n'
