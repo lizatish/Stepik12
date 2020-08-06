b46ea5b26f10192950135c4994680f8468c20500
Liza Tishkovets <tiiska2@gmail.com>
2020-08-06 15:15:29 +0300

    Second time

--- hello.py
+++ hello.py
@@ -1,8 +1,7 @@
-def wsgi_appliction(environ, start_responce):
-    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+def wsgi_appliction(environ, start_response):
     status = '200 OK'
-    headers = [
-        ('Content-Type', 'text/plain')
-    ]
-    start_responce(status, headers)
+    headers = [('Content-Type', 'text/plain')]
+    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+
+    start_response(status, headers)
     return body
