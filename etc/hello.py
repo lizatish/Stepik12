ba940ecedcfc97ef295d9f389432dc74296e7932
Liza Tishkovets <tiiska2@gmail.com>
2020-08-06 15:06:07 +0300

    Second time

--- etc/hello.py
+++ etc/hello.py
@@ -2,7 +2,7 @@ CONFIG = {
     'mode': 'wsgi',
     'working_dir': '/home/box/web/',
     'args': (
-        'bind=0.0.0.0:8080',
+        '--bind=0.0.0.0:8080',
         '--workers=4',
         '--timeout=60',
         'hello:wsgi_appliction',
--- hello.py
+++ hello.py
@@ -1,10 +1,14 @@
 def wsgi_appliction(environ, start_responce):
+    raw_uri = str(environ.get('RAW_URI'))
+    raw_uri = raw_uri[2:]
+    params = raw_uri.split('&')
 
-    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
-    status = '200 OK'
-    headers = [
-        ('Content-Type', 'text/plain')
-    ]
+    data = ''
+    for param in params:
+        data += param + '\r\n'
 
-    start_responce(status, headers)
-    return body
+    start_responce("200 OK", [
+        ("Content-Type", "text/plain"),
+        ("Content-Length", str(len(data)))
+    ])
+    return iter([data])
