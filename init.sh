d37ff41a32350d526763438cc4e35d77900b3522
Elizaveta Tishkovets <tiishka2@gmail.com>
2020-08-11 19:08:17 +0300

    Change init.sh

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ sudo ln -s /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
 
 sudo ln -s /home/box/web/etc/hello_django.py  /etc/gunicorn.d/hello_django.py
+sudo gunicorn -c /etc/gunicorn.d/hello_django.py ask.wsgi:application
 sudo /etc/init.d/gunicorn restart
 
 
