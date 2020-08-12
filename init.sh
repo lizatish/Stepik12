35eedab6d0bdc91720b263544c22dc2aa4cb1aea
Elizaveta Tishkovets <tiishka2@gmail.com>
2020-08-12 16:06:06 +0300

    Changes2

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-sudo ln -sf /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
-sudo ln -sf /home/box/web/etc/hello_django.conf  /etc/gunicorn.d/hello_django.conf
+sudo ln -s /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
 
+sudo ln -s /home/box/web/etc/hello_django.py  /etc/gunicorn.d/hello_django.py
+sudo /etc/init.d/gunicorn restart
