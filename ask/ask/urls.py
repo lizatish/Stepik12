a401824533c69803d58fcab66e8e5ef3a4e500cf
Elizaveta Tishkovets <tiishka2@gmail.com>
2020-08-12 17:21:29 +0300

    Python 3.5

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,6 +1,21 @@
 """ask URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+"""ask URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
@@ -13,13 +28,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-"""from django.urls import path, include, re_path"""
+from django.urls import path, include, re_path
 urlpatterns = [
-    url(r'^$', include('qa.urls')),
-    url(r'^login/', include('qa.urls'), name='login'),
-    url(r'^signup/', include('qa.urls'), name='signup'),
-    url(r'^question/(?P<id>[0-9]+)/$', include('qa.urls'), name='question'),
-    url('^ask/', include('qa.urls'), name='ask'),
-    url('^popular/', include('qa.urls'), name='popular'),
-    url(r'^new/', include('qa.urls'), name='new')
+    re_path(r'^$', include('qa.urls')),
+    path('login/', include('qa.urls'), name='login'),
+    path('signup/', include('qa.urls'), name='signup'),
+    re_path(r'^question/(?P<id>[0-9]+)/$', include('qa.urls'), name='question'),
+    path('ask/', include('qa.urls'), name='ask'),
+    path('popular/', include('qa.urls'), name='popular'),
+    path('new/', include('qa.urls'), name='new')
 ]
---
+++ venv2/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/liza/Stepik/venv2/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
