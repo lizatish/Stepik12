4f208c357895f8085a3e2d63479b49d137b370ab
Elizaveta Tishkovets <tiishka2@gmail.com>
2020-08-14 13:12:35 +0300

    Add database mysql

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
+class Question(models.Model):
+    objects = QuestionManager()
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateField()
+    rating = models.IntegerField()
+    author = models.CharField(max_length=255)
+    likes = models.ManyToManyField(User)
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField()
+    question = models.OneToOneField(
+        Question,
+        on_delete=models.CASCADE,
+    )
+    author = models.CharField(max_length=255)
