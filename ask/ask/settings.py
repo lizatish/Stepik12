2777b4642b8da2e6bcf56339a389fcf1a7ccab74
Elizaveta Tishkovets <tiishka2@gmail.com>
2020-08-14 16:28:39 +0300

    Add database mysql

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -76,12 +76,9 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-
     'default': {
-
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'stepic_web',
-        'USER': 'box',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
