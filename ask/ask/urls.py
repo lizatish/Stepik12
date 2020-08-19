1e3f6427e9c3ccd3fbb050eac68776893ae36416
Elizaveta Tishkovets <tiishka2@gmail.com>
2020-08-19 12:04:59 +0300

    Change urls

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -32,14 +32,14 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'', include('qa.urls')),
+    # url(r'', include('qa.urls')),
 
-    # url(r'^$', include('qa.urls')),
-    # url(r'^login/', include('qa.urls')),
-    # url(r'^signup/', include('qa.urls')),
-    # url(r'^ask/', include('qa.urls')),
-    # url(r'^popular/', include('qa.urls')),
-    # url(r'^new/', include('qa.urls')),
-    # url(r'^admin/', admin.site.urls),
-    # url(r'^question/', include('qa.urls')),
+    url(r'^$', include('qa.urls')),
+    url(r'^login/', include('qa.urls')),
+    url(r'^signup/', include('qa.urls')),
+    url(r'^ask/', include('qa.urls')),
+    url(r'^popular/', include('qa.urls')),
+    url(r'^new/', include('qa.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^question/', include('qa.urls')),
 ]
