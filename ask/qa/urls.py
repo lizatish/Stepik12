3b69146fd920956244f3299d3111fd231e975b8e
Elizaveta Tishkovets <tiishka2@gmail.com>
2020-08-19 12:06:06 +0300

    Change urls

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -17,22 +17,22 @@ from django.conf.urls import url, include, re_path
 from . import views
 # from django.urls import path
 
-urlpatterns = [
-    url(r'^$', views.new_questions),
-    url('login/', views.test, name='login'),
-    url('signup/', views.test, name='signup'),
-    url(r'^question/(?P<id>[0-9]+)/$', views.question, name='question'),
-    url('ask/', views.test, name='ask'),
-    url('popular/', views.popular_questions, name='popular'),
-    url('new/', views.test, name='new')
-]
-
 # urlpatterns = [
 #     url(r'^$', views.new_questions),
-#     url(r'^login/', views.test, name='login'),
-#     url(r'^signup/', views.test, name='signup'),
+#     url('login/', views.test, name='login'),
+#     url('signup/', views.test, name='signup'),
 #     url(r'^question/(?P<id>[0-9]+)/$', views.question, name='question'),
-#     url(r'^ask/', views.test, name='ask'),
-#     url(r'^popular/', views.popular_questions, name='popular'),
-#     url(r'^new/', views.test, name='new')
+#     url('ask/', views.test, name='ask'),
+#     url('popular/', views.popular_questions, name='popular'),
+#     url('new/', views.test, name='new')
 # ]
+
+urlpatterns = [
+    url(r'^$', views.new_questions),
+    url(r'^login/', views.test, name='login'),
+    url(r'^signup/', views.test, name='signup'),
+    url(r'^question/(?P<id>[0-9]+)/$', views.question, name='question'),
+    url(r'^ask/', views.test, name='ask'),
+    url(r'^popular/', views.popular_questions, name='popular'),
+    url(r'^new/', views.test, name='new')
+]
